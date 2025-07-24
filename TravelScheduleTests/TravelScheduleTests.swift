aaafe81567a9fa3b9b8b66f8a58fc14078ee040b
Dmitrii Seitsman <seytsman@gmail.com>
2025-07-24 11:04:56 +0300

    Initial commit

---
+++ TravelScheduleTests/TravelScheduleTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleTests.swift
+//  TravelScheduleTests
+//
+//  Created by Dmitrii Seitsman on 24.07.2025.
+//
+
+import Testing
+@testable import TravelSchedule
+
+struct TravelScheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
