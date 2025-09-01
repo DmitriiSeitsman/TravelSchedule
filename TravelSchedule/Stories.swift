abe9a9e671aecb03b1baa3e11882d5008571c615
Dmitrii Seitsman <seytsman@gmail.com>
2025-09-01 14:45:40 +0300

    добавлены stories

---
+++ TravelSchedule/Stories.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct Stories {
+    let backgroundColor: Color
+    let title: String
+    let description: String
+
+    static let story1 = Stories(
+        backgroundColor: .story1Background,
+        title: "🎉 ⭐️ ❤️",
+        description: "Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 Text1 "
+    )
+
+    static let story2 = Stories(
+        backgroundColor: .story2Background,
+        title: "😍 🌸 🥬",
+        description: "Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 Text2 "
+    )
+
+    static let story3 = Stories(
+        backgroundColor: .story3Background,
+        title: "🧀 🥑 🥚",
+        description: "Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 Text3 "
+    )
+}
