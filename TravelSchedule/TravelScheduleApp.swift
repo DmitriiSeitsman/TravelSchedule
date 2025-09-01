4af896cbc236c15dd478224ee26572d33a844793
Dmitrii Seitsman <seytsman@gmail.com>
2025-09-01 15:26:38 +0300

    Свёрстан экран настроек, реализовано переключение темы (светлая/тёмная)

--- TravelSchedule/TravelScheduleApp.swift
+++ TravelSchedule/TravelScheduleApp.swift
@@ -2,9 +2,13 @@ import SwiftUI
 
 @main
 struct TravelScheduleApp: App {
+    @AppStorage("isDarkMode") private var isDarkMode = false
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(isDarkMode ? .dark : .light)
         }
     }
 }
+
